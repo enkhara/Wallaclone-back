43a89968f8f95ba02f8649f41803d78a51f449b1
jaime Andres Triana <zubov2000@gmail.com>
2021-07-02 12:44:57 +0200

    add: connection to database, with its environment variable, file (.env) fix: package.json "dev"

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+require('./lib/connectMongoose');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -28,14 +30,35 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
+  
+
+  if (err.array) {
+    const errorInfo = err.array({ onlyFirstError: true })[0];
+    err.message = `Not valid - ${errorInfo.param} ${errorInfo.msg}`;
+    err.status = 422;
+  }
+  res.status(err.status || 500);
+
+  if (isAPIRequest(req)) {
+    res.json({ eror: err.message });
+    return;
+  }
+
   // set locals, only providing error in development
+
+
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  
   res.render('error');
 });
+
+function isAPIRequest(req) {
+  return req.originalUrl.indexOf('/api/') === 0;
+}
 
 module.exports = app;
