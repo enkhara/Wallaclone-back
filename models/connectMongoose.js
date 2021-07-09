91253ab7f3782313757be9c0eef7c712e650d739
Georgina Torralba <georgina.torralba@gmail.com>
2021-07-09 15:21:41 +0200

    Move connectMongoose.js from lib to models

--- models/connectMongoose.js
+++ models/connectMongoose.js
@@ -3,19 +3,18 @@
 const mongoose = require('mongoose');
 
 mongoose.connection.on('error', (err) => {
-  console.log('Error de conexión', err);
-
-  process.exit(1);
+	console.log('Error de conexión', err);
+	process.exit(1);
 });
 
 mongoose.connection.once('open', () => {
-  console.log('Conectado a MongoDB en', mongoose.connection.name);
+	console.log('Conectado a MongoDB en', mongoose.connection.name);
 });
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STR, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
 });
 
 module.exports = mongoose.connection;
