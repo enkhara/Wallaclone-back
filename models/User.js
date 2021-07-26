30b77767b914fd84a2c88d7e535e6835efbc0f94
jaime Andres Triana <zubov2000@gmail.com>
2021-07-26 21:38:01 +0200

    modif: user in models

--- models/User.js
+++ models/User.js
@@ -8,14 +8,13 @@ const emailTransportConfigure = require('../lib/emailTransportConfigure');
 const userSchema = mongoose.Schema({
   username: { type: String, unique: true, index: true },
   email: { type: String, unique: true, index: true },
-  password: String,   
-  ads_favs: [{
-     type: mongoose.Schema.Types.ObjectId,
-     ref: 'Advertisement'
-   }]
-},
- {
-  collection: 'users'  
+  password: String,
+  ads_favs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Advertisement',
+    },
+  ],
 });
 
 userSchema.statics.hashPassword = function (passwordEnClaro) {
@@ -29,15 +28,14 @@ userSchema.methods.comparePassword = function (passwordEnClaro) {
 
 // En los métodos de mongoose no usar Arrow Functions para no tener problemas con el this
 // es un método que no está dentro de mongoose
-userSchema.statics.lista = async function (filtro, limit, skip, fields, sort){
-    const query = User.find(filtro); // no devuelve una promesa, devuelve una query que tiene un método then
-    query.limit(limit);
-    query.skip(skip);
-    query.select(fields);
-    query.sort(sort);
-    
-   return query.exec(); // devuelve una promesa
-    
+userSchema.statics.lista = async function (filtro, limit, skip, fields, sort) {
+  const query = User.find(filtro); // no devuelve una promesa, devuelve una query que tiene un método then
+  query.limit(limit);
+  query.skip(skip);
+  query.select(fields);
+  query.sort(sort);
+
+  return query.exec(); // devuelve una promesa
 };
 
 const User = mongoose.model('User', userSchema);
