1809ed492b51d01c01e092df7e3986da1e9b1890
Georgina Torralba <georgina.torralba@gmail.com>
2021-08-03 01:11:08 +0200

    Created messages post and get

--- routes/messages.js
+++ routes/messages.js
@@ -5,18 +5,42 @@ const Message = require('../models/Message');
 
 //Add
 
-router.post('/', async (res, req, next) => {
-	const newMessage = new Message(req.body);
+router.post('/', async (req, res, next) => {
+	const { conversationId, sender, text } = req.body;
+	const newMessage = new Message({ conversationId, sender, text });
 	console.log(req.body);
-	console.log(newMessage);
+	//console.log('nuevo mensage', newMessage);
 
 	try {
 		const saveMessage = await newMessage.save();
 		console.log(saveMessage);
 		res.status(200).json(saveMessage);
 	} catch (err) {
+		res.status(500).json(err);
 		next(err);
 	}
 });
+
+router.get('/:conversationId', async (req, res, next) => {
+	try {
+		console.log(req.params);
+		const messages = await Message.find({
+			conversationId: req.params.conversationId,
+		});
+		res.status(200).json(messages);
+	} catch (err) {
+		res.status(500).json(err);
+		next(err);
+	}
+});
+
+// router.get('/', async function (req, res, next) {
+// 	try {
+// 		const resultado = await Message.find();
+// 		res.json(resultado);
+// 	} catch (err) {
+// 		next(err);
+// 	}
+// });
 
 module.exports = router;
