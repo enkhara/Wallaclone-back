57e2692d572b380701bc30f219884e4671d5b7d4
Georgina Torralba <georgina.torralba@gmail.com>
2021-08-15 16:17:23 +0200

    Function get('/users/;userId', for get speakers

--- routes/conversations.js
+++ routes/conversations.js
@@ -6,7 +6,7 @@ const Conversation = require('../models/Conversation');
 // new Conversation
 
 router.post('/', async (req, res, next) => {
-	console.log(req.body);
+	//console.log(req.body);
 	const { advertisementId, senderId, receiverId } = req.body;
 	const newConversation = new Conversation({
 		advertisementId,
@@ -29,11 +29,11 @@ router.post('/', async (req, res, next) => {
 router.get('/userConversations/:userId', async (req, res, next) => {
 	try {
 		const userId = req.params.userId;
-		console.log('userId', userId);
+		//console.log('userId', userId);
 		const conversation = await Conversation.find({
 			members: { $in: [userId] },
 		});
-		console.log('userConversations', conversation);
+		//console.log('userConversations', conversation);
 		res.status(200).json(conversation);
 	} catch (err) {
 		next(err);
@@ -42,8 +42,21 @@ router.get('/userConversations/:userId', async (req, res, next) => {
 
 router.get('/users/:userId', async (req, res, next) => {
 	try {
-		const users = await Conversation.find({ members: { $in: ![userId] } });
-		console.log(users);
+		const userId = req.params.userId;
+		console.log('userId', userId);
+		const users = await Conversation.find({ members: { $in: [userId] } });
+		console.log('Array de users', users);
+		let speakers = users.map((user) => {
+			console.log('user in map', user.members);
+			return user.members.find((member) => member !== userId);
+		});
+		console.log(
+			'linea 49 conversations.js en speakers para usuarios conectados',
+			speakers
+		);
+		const speakersUniques = new Set(speakers);
+		let result = [...speakersUniques];
+		res.status(200).json(result);
 	} catch (err) {
 		next(err);
 	}
@@ -51,14 +64,14 @@ router.get('/users/:userId', async (req, res, next) => {
 
 router.get('/:userId/:advertisementId', async (req, res, next) => {
 	try {
-		console.log('userid', req.params);
+		//console.log('userid', req.params);
 		const { userId, advertisementId } = req.params;
 		const conversation = await Conversation.find({
 			advertisementId,
 			members: { $in: [userId] },
 			//members: { $in: [req.params.userId] },
 			//BUSCAR COMO FILTRAR POR DOS CAMPOS LISTA ANUNCIOS advertisements.js
-		}).populate({ path: 'advertisementId' });;
+		}).populate({ path: 'advertisementId' });
 		res.status(200).json(conversation);
 	} catch (err) {
 		next(err);
